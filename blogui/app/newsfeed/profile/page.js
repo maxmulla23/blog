1a80ff93775ec15ce6c1dc7fad70e514e815f651
maxmulla23 <maxmulla23@gmail.com>
2025-03-13 13:31:53 +0300

    user

--- blogui/app/newsfeed/profile/page.js
+++ blogui/app/newsfeed/profile/page.js
@@ -1,5 +1,22 @@
+import { Card, CardContent } from "@/components/ui/card";
+import { UserCircleIcon } from '@heroicons/react/24/solid'
+
+const User = {
+    name: "Maxwell Mulla",
+    email: "maxmulla23@gmail.com",
+
+}
+
 export default function Page() {
     return(
-        <h1>This is the profile page</h1>
+        <div>
+            <Card className="h-auto w-screen mt-0">
+                <CardContent className="px-10 pt-10 text-cyan-900">
+                <UserCircleIcon className=" h-52 w-auto" />
+                </CardContent>
+            </Card>
+        </div>
+        
+
     )
 }
