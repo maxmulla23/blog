4e29fd5ba1ba81b6be15e5bdf3ce2224bb598e69
maxmulla23 <maxmulla23@gmail.com>
2025-03-20 12:02:22 +0300

    profile

--- blogui/app/newsfeed/profile/page.js
+++ blogui/app/newsfeed/profile/page.js
@@ -1,3 +1,4 @@
+import { BlogCard } from "@/app/comp/blogCard";
 import { Card, CardContent } from "@/components/ui/card";
 import { UserCircleIcon } from '@heroicons/react/24/solid'
 
@@ -9,19 +10,23 @@ const User = {
 
 export default function Page() {
     return(
-        <div>
-            <Card className="h-auto w-screen mt-0">
-            <div className="flex flex-col gap-3">
+        <div className="flex justify-center">
+            <Card className="h-auto w-screen shadow-lg mt-0">
+            
                 <CardContent className="text-cyan-900">
                 <UserCircleIcon className=" h-52 w-auto" />
-                <div >
+                <div className="flex flex-col ml-2">
                 <h1 className="uppercase text-4xl">{User.name}</h1>
                 <h2 className="text-lg">{User.email}</h2>
+                </div>
+                <hr className="mt-6 border-gray-800 " />
+                <div className="flex items-center justify-center mt-5">
+                    <BlogCard />
                 </div>
                
                 
                 </CardContent>
-                </div>
+                
             </Card>
         </div>
         
