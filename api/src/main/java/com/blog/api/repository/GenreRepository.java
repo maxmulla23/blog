e05a1d53a068e7d3c1cbe2e617af70ab1bee7a1d
maxmulla23 <maxmulla23@gmail.com>
2025-03-22 03:19:35 +0300

    repo

--- api/src/main/java/com/blog/api/repository/GenreRepository.java
+++ api/src/main/java/com/blog/api/repository/GenreRepository.java
@@ -1,9 +1,12 @@
 package com.blog.api.repository;
 
 import com.blog.api.models.Genre;
+import com.blog.api.models.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface GenreRepository extends JpaRepository<Genre, Integer> {
+    Optional<Genre> findByName(String name);
 }
