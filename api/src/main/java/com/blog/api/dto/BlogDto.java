5aeb5acef7bf8bcdb3b40a4559cc9c2b84f988c3
maxmulla23 <maxmulla23@gmail.com>
2025-03-26 10:47:56 +0300

    constructor

--- api/src/main/java/com/blog/api/dto/BlogDto.java
+++ api/src/main/java/com/blog/api/dto/BlogDto.java
@@ -2,9 +2,26 @@ package com.blog.api.dto;
 
 import lombok.Data;
 
+import java.time.LocalDateTime;
+
 @Data
 public class BlogDto {
     private int id;
     private String title;
     private String body;
+    private String createdBy;
+    private LocalDateTime createdAt;
+    private LocalDateTime lastModifiedDate;
+
+    public BlogDto() {
+    }
+
+    public BlogDto(int id, String title, String body, String createdBy, LocalDateTime createdAt, LocalDateTime lastModifiedDate) {
+        this.id = id;
+        this.title = title;
+        this.body = body;
+        this.createdBy = createdBy;
+        this.createdAt = createdAt;
+        this.lastModifiedDate = lastModifiedDate;
+    }
 }
