08d57817c64775ceb129f0d00a1078e3895c2658
maxmulla23 <maxmulla23@gmail.com>
2025-04-02 09:29:32 +0300

    repo

--- api/src/main/java/com/blog/api/repository/BlogRepository.java
+++ api/src/main/java/com/blog/api/repository/BlogRepository.java
@@ -7,11 +7,12 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 
+import java.util.List;
 import java.util.Optional;
 
 public interface BlogRepository extends JpaRepository<Blog, Integer> {
-    Page<Blog> findByUserOrderByCreatedDateDesc(UserEntity user, Pageable pageable);
-    Page<Blog> findAllByOrderByCreatedDateDesc(Pageable pageable);
+    List<Blog> findByUser(UserEntity user);
+    List<Blog> findByGenreId(int genreId);
 
     Optional<Blog> findById(int id);
 
