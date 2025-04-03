34749811f1813c59a4a2a778947a1cddbf8d4ba3
maxmulla23 <maxmulla23@gmail.com>
2025-04-03 11:51:53 +0300

    comment

---
+++ api/src/main/java/com/blog/api/dto/CreateCommentDTO.java
@@ -0,0 +1,8 @@
+package com.blog.api.dto;
+
+import lombok.Data;
+
+@Data
+public class CreateCommentDTO {
+    private String content;
+}
--- api/src/main/java/com/blog/api/repository/CommentRepository.java
+++ api/src/main/java/com/blog/api/repository/CommentRepository.java
@@ -3,5 +3,9 @@ package com.blog.api.repository;
 import com.blog.api.models.Comment;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface CommentRepository extends JpaRepository<Comment, Integer> {
+    List<Comment> findByBlogs(int blogId);
 }
