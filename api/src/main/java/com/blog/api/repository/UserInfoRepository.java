32207613693a94c907dade0e110b975a55616e8d
maxmulla23 <maxmulla23@gmail.com>
2025-04-09 11:50:33 +0300

    repo

---
+++ api/src/main/java/com/blog/api/repository/UserInfoRepository.java
@@ -0,0 +1,11 @@
+package com.blog.api.repository;
+
+import com.blog.api.models.UserEntity;
+import com.blog.api.models.UserInfo;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserInfoRepository extends JpaRepository<UserInfo, Integer> {
+    Optional<UserInfo> findByUser(UserEntity user);
+}
