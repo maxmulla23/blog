3f5a668c6913dad490a54a733f4bc7325558a1d1
maxmulla23 <maxmulla23@gmail.com>
2025-04-09 13:56:21 +0300

    get service

--- api/src/main/java/com/blog/api/service/UserInfoService.java
+++ api/src/main/java/com/blog/api/service/UserInfoService.java
@@ -1,11 +1,16 @@
 package com.blog.api.service;
 
+import com.blog.api.dto.UserInfoDto;
 import com.blog.api.models.UserEntity;
 import com.blog.api.models.UserInfo;
 import com.blog.api.repository.UserInfoRepository;
 import com.blog.api.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 public class UserInfoService {
@@ -20,5 +25,29 @@ public class UserInfoService {
         userInfo.setUser(user);
 
         return userInfoRepository.save(userInfo);
+    }
+
+    public UserInfoDto getInfoByUser(UserEntity user) {
+        Optional<UserInfo> userInfo1 = userInfoRepository.findByUser(user);
+        return userInfo1.map(this::mapToDto).orElseThrow(() -> new RuntimeException("not found" +user.getUsername()));
+    }
+
+    private UserInfoDto mapToDto(UserInfo userInfo) {
+        UserInfoDto userInfoDto = new UserInfoDto();
+        userInfoDto.setId(userInfo.getId());
+        userInfoDto.setFirstName(userInfo.getFirstName());
+        userInfoDto.setLastName(userInfo.getLastName());
+        userInfoDto.setEmail(userInfo.getEmail());
+        userInfoDto.setBio(userInfo.getBio());
+        return userInfoDto;
+    }
+    private UserInfo mapToEntity(UserInfoDto userInfoDto) {
+        UserInfo userInfo = new UserInfo();
+        userInfo.setId(userInfoDto.getId());
+        userInfo.setFirstName(userInfoDto.getFirstName());
+        userInfo.setLastName(userInfoDto.getLastName());
+        userInfo.setEmail(userInfoDto.getEmail());
+        userInfo.setBio(userInfoDto.getBio());
+        return userInfo;
     }
 }
