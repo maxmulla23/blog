374b7c4698dd32b5f34c0dff42d003a5e3342aed
maxmulla23 <maxmulla23@gmail.com>
2025-04-09 13:55:58 +0300

    optional

--- api/src/main/java/com/blog/api/repository/UserInfoRepository.java
+++ api/src/main/java/com/blog/api/repository/UserInfoRepository.java
@@ -4,6 +4,7 @@ import com.blog.api.models.UserEntity;
 import com.blog.api.models.UserInfo;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface UserInfoRepository extends JpaRepository<UserInfo, Integer> {
